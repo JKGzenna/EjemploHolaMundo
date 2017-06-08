2ae4621d7d792ecc69eddf8d4e02c84790758dce
JKGzenna <juanlu360@gmail.com>
2017-06-09 01:19:54 +0200

    HolaMundo

---
+++ src/ejemploholamundo/EjemploHolaMundo.java
@@ -0,0 +1,13 @@
+package ejemploholamundo;
+
+/**
+ *
+ * @author JUAN
+ */
+public class EjemploHolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+    }
+    
+}
